bef669a036093390c6d4399479cb50457ddc1075
Edgar24k <56399581+Edgar24k@users.noreply.github.com>
2021-06-17 15:52:27 -0500

    Add files via upload

---
+++ proyecto/otro/admin/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('RUTA', 'http://localhost/login_curso/');
+
+// nombre de base de datos
+$bd_config = array(
+    'db_name' => 'login_curso',
+    'user' => 'root',
+    'pass' => ''    
+);
+
+
+ ?>
